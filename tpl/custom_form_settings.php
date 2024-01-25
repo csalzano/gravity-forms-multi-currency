6da695a708c877273b749e6ff830fe2a1eb595c8
Corey Salzano <938411+csalzano@users.noreply.github.com>
2024-01-25 14:29:56 -0500

    Escapes text for output. Adds support for translations.

--- tpl/custom_form_settings.php
+++ tpl/custom_form_settings.php
@@ -1,17 +1,11 @@
 		<li>
-			<label for="form_currency">Currency</label>
+			<label for="form_currency"><?php esc_html_e( 'Currency', 'gravity-forms-multi-currency' ); ?></label>
 			<select id="form_currency" name="form_currency" style="width:200px">
-				<option value="">Default currency (<?php echo $this->gf_get_default_currency(); ?>)</option>
+				<option value=""><?php esc_html_e( 'Default currency', 'gravity-forms-multi-currency' ); ?> (<?php echo esc_html( $this->gf_get_default_currency() ); ?>)</option>
 				<?php
 				foreach ( RGCurrency::get_currencies() as $code => $currency ) :
 					?>
-				<option
-					<?php
-					if ( rgar( $form, 'currency' ) === $code ) :
-						?>
-					selected="selected"
-					<?php endif; ?>
-					value="<?php echo $code; ?>"><?php echo $currency['name']; ?></option>
+				<option <?php selected( rgar( $form, 'currency' ), $code, true ); ?> value="<?php echo esc_attr( $code ); ?>"><?php echo esc_html( $currency['name'] ); ?></option>
 				<?php endforeach; ?>
 			</select>
 		</li>
